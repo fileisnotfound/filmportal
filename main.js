2a3981f2e79fca5c5205544aa4d1df4bb6d70246
fileisnotfound <94240011+fileisnotfound@users.noreply.github.com>
2021-11-13 18:24:52 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+var iframeDocument = document.getElementById("iframe");
+var img1 = document.getElementById("img1");
+var img2 = document.getElementById("img2");
+var img3 = document.getElementById("img3");
+var img4 = document.getElementById("img4");
+var nameText = document.getElementById("actorNames");
+var theExpendables = document.getElementById("theExpendables");
+theExpendables.onclick = function () {
+    iframeDocument.src = "https://www.youtube.com/embed/8KtYRALe-xo";
+    img1.src = "https://upload.wikimedia.org/wikipedia/commons/8/84/Sylvester_Stallone_Cannes_2019.jpg";
+    img2.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Jason_Statham_2007.jpg/180px-Jason_Statham_2007.jpg";
+    img3.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/Dolph_Lundgren_2018.jpg/250px-Dolph_Lundgren_2018.jpg";
+    img4.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Randy_Couture_by_Gage_Skidmore.jpg/250px-Randy_Couture_by_Gage_Skidmore.jpg";
+    nameText.innerHTML=("&nbsp;&nbsp;Sylvester Stalone&emsp;&nbsp;&emsp;&nbsp;Jason Statham&emsp;&nbsp;&emsp;&nbsp;Dolph Lundgren &nbsp;&emsp;&emsp;&nbsp;Randy Couture");
+};
+var theFastAndTheFurious = document.getElementById("theFastAndTheFurious");
+theFastAndTheFurious.onclick = function () {
+    iframeDocument.src = "https://www.youtube.com/embed/2TAOizOnNPo";
+    img1.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Vin_Diesel_XXX_Return_of_Xander_Cage_premiere.png/250px-Vin_Diesel_XXX_Return_of_Xander_Cage_premiere.png";
+    img2.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/PaulWalkerEdit-1.jpg/250px-PaulWalkerEdit-1.jpg";
+    img3.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/Michelle_Rodriguez_Cannes_2018_cropped.jpg/250px-Michelle_Rodriguez_Cannes_2018_cropped.jpg";
+    img4.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNQpmTC5rwHIep3t02OTOur4HBcd46rdXiZftnzR1eapDFtgp1-ukzlDoDFcgtwTlzF8s&usqp=CAU";
+    nameText.innerHTML=("&nbsp;&nbsp;&emsp;&emsp;Vin Diesel&emsp;&emsp;&nbsp;&emsp;&nbsp;&nbsp;&nbsp;&nbsp;&emsp;Paul Walker&emsp;&emsp;&nbsp;&emsp;&nbsp;Michelle Rodriguez&nbsp;&nbsp;&emsp;&nbsp;Paloma Jiménez");
+};  
+var swat = document.getElementById("swat");
+swat.onclick = function () {
+    iframeDocument.src = "https://www.youtube.com/embed/7IoGHLXWs28";
+    img1.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Colin_Farrell_by_Gage_Skidmore.jpg/250px-Colin_Farrell_by_Gage_Skidmore.jpg";
+    img2.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/Michelle_Rodriguez_Cannes_2018_cropped.jpg/250px-Michelle_Rodriguez_Cannes_2018_cropped.jpg";
+    img3.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Jeremy_Renner_by_Gage_Skidmore.jpg/250px-Jeremy_Renner_by_Gage_Skidmore.jpg";
+    img4.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Samuel_L._Jackson_2019_by_Glenn_Francis.jpg/250px-Samuel_L._Jackson_2019_by_Glenn_Francis.jpg";
+    nameText.innerHTML=("&nbsp;&nbsp;&emsp;Colin Farrel&emsp;&nbsp;&emsp;&nbsp;&emsp;Michelle Rodriguez&emsp;&nbsp;&emsp;&nbsp;Jeremy Renner&nbsp;&nbsp;&emsp;&nbsp;Samuel L. Jackson");
+};
+var johnWick = document.getElementById("johnWick");
+johnWick.onclick = function () {
+    iframeDocument.src = "https://www.youtube.com/embed/C0BMx-qxsP4";
+    img1.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Reuni%C3%A3o_com_o_ator_norte-americano_Keanu_Reeves_%28cropped%29.jpg/250px-Reuni%C3%A3o_com_o_ator_norte-americano_Keanu_Reeves_%28cropped%29.jpg";
+    img2.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Chad_Stahelski.jpg/250px-Chad_Stahelski.jpg";
+    img3.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/IanMcShane06.jpg/250px-IanMcShane06.jpg";
+    img4.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Lance_Reddick_by_Gage_Skidmore.jpg/250px-Lance_Reddick_by_Gage_Skidmore.jpg";
+    nameText.innerHTML=("&nbsp;&nbsp;&emsp;Keanu Reeves&emsp;&nbsp;&emsp;&nbsp;&emsp;Chad Stahelski&emsp;&nbsp;&emsp;&nbsp;&emsp;Ian McShane&nbsp;&nbsp;&emsp;&nbsp;&nbsp;&nbsp;&emsp;&emsp;Lance Reddick");
+};
+var escapePlan = document.getElementById("escapePlan");
+escapePlan.onclick = function () {
+    iframeDocument.src = "https://www.youtube.com/embed/CI4EjV_x_PQ";
+    img1.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Arnold_Schwarzenegger_by_Gage_Skidmore_4.jpg/225px-Arnold_Schwarzenegger_by_Gage_Skidmore_4.jpg";
+    img2.src = "https://upload.wikimedia.org/wikipedia/commons/8/84/Sylvester_Stallone_Cannes_2019.jpg";
+    img3.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Dave_Bautista_Photo_Op_GalaxyCon_Minneapolis_2019.jpg/250px-Dave_Bautista_Photo_Op_GalaxyCon_Minneapolis_2019.jpg";
+    img4.src = "https://upload.wikimedia.org/wikipedia/commons/a/a3/50_Cent_AKA_Curtis_Jackson_%28cropped%29.jpg";
+    nameText.innerHTML=("Arnold Schwarzenegger&nbsp;Sylvester Stallone&emsp;&nbsp;&nbsp;&nbsp;&nbsp;Dave Bautista&nbsp;&nbsp;&emsp;&nbsp;&nbsp;&nbsp;&emsp;&emsp;50 Cent");
+};
